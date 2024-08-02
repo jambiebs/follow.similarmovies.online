68872c9f6de2a4a4868bc6fe8028f749a2a16b8a
jam_al <86470413+jambiebs@users.noreply.github.com>
2024-08-03 03:51:11 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,3 @@
-// Import Firebase libraries
-importScripts('./firebase-app.js');
-importScripts('./firebase-messaging.js');
-
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCvAshe0Yvqik3Y5roqdA0T4ZqwyYKhRCI",
-  authDomain: "similarmovies-bb176.firebaseapp.com",
-  projectId: "similarmovies-bb176",
-  storageBucket: "similarmovies-bb176.appspot.com",
-  messagingSenderId: "520740462999",
-  appId: "1:520740462999:web:d8f419b7fc23b6166ca4d2",
-  measurementId: "G-M9HC31VTFM"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
 
