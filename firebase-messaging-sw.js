3ee4aa4f5dccd73e1bdb863d1a8b99e1b1b8013f
jam_al <86470413+jambiebs@users.noreply.github.com>
2024-08-03 01:07:36 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,41 +17,36 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Handle background messages
-messaging.onBackgroundMessage(function(payload) {
-  console.log('Received background message ', payload);
+self.addEventListener('push', (event) => {
+  if (event.data) {
+    const data = event.data.json();
+    const title = data.title;
+    const options = {
+      body: data.body,
+      icon: '/hm-icon-192x192.png',
+      data: {
+        notifURL: data.notifURL || '/',
+      }
+    };
 
-  // Extract the notification data from the payload
-  const { title, body, click_action } = payload.data;
-
-  // Customize notification options
-  const notificationOptions = {
-    body: body,
-    icon: '/hm-icon-192x192.png', // Optional icon
-    data: {
-      url: click_action
-    }
-  };
-
-  // Show the notification
-  self.registration.showNotification(title, notificationOptions);
+    event.waitUntil(
+      self.registration.showNotification(title, options)
+    );
+  }
 });
 
-// Handle notification click events
-self.addEventListener('notificationclick', function(event) {
-  const notification = event.notification;
-  const urlToOpen = notification.data.url || '/';
-
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close(); // Close the notification
+  const url = event.notification.data.notifURL || '/';
   event.waitUntil(
-    clients.matchAll({ type: 'window' }).then(function(clientList) {
-      for (let i = 0; i < clientList.length; i++) {
-        const client = clientList[i];
-        if (client.url === urlToOpen && 'focus' in client) {
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if (client.url === url && 'focus' in client) {
           return client.focus();
         }
       }
       if (clients.openWindow) {
-        return clients.openWindow(urlToOpen);
+        return clients.openWindow(url);
       }
     })
   );
