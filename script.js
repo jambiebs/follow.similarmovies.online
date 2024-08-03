d16f8d8a420c84082899ff2f28051e6201f107c1
jam_al <86470413+jambiebs@users.noreply.github.com>
2024-08-03 20:03:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,4 @@
-// function setCookie(name, value, days, domain) {
-//   var expires = "";
-//   if (days) {
-//     var date = new Date();
-//     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-//     expires = "; expires=" + date.toUTCString();
-//   }
-//   document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=" + domain;
-// }
-
-// function getCookie(name) {
-//   var nameEQ = name + "=";
-//   var ca = document.cookie.split(';');
-//   for (var i = 0; i < ca.length; i++) {
-//     var c = ca[i];
-//     while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-//     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-//   }
-//   return null;
-// }
-
-// function deleteCookie(name, domain) {
-//   document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=" + domain;
-// }
-
-// function checkNotificationAndUpdateButton() {
-//   const actionButton = document.getElementById('actionButton');
-//   if (getCookie("hm-notify") === "true") {
-//     actionButton.textContent = "❮ Go Back";
-//     actionButton.onclick = function() {
-//       window.history.back();
-//     };
-//   } else {
-//     actionButton.textContent = "Subscribe";
-//     actionButton.onclick = function() {
-//       Notification.requestPermission().then(permission => {
-//         if (permission === "granted") {
-//           setCookie("hm-notify", "true", 365, ".similarmovies.online");
-//           actionButton.textContent = "❮ Go Back";
-//           actionButton.onclick = function() {
-//             window.history.back();
-//           };
-//         } else {
-//           console.log('Notification permission denied');
-//         }
-//       }).catch((error) => {
-//         console.error('Error requesting notification permission:', error);
-//       });
-//     };
-//   }
-// }
-
-// document.addEventListener('DOMContentLoaded', checkNotificationAndUpdateButton);
-// setInterval(checkNotificationAndUpdateButton, 1000);
-// Function to set a cookie
+// set a cookie
 function setCookie(name, value, days, domain) {
   var expires = "";
   if (days) {
@@ -61,39 +7,31 @@ function setCookie(name, value, days, domain) {
     expires = "; expires=" + date.toUTCString();
   }
   document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=" + domain;
-  console.log(`Cookie set: ${name}=${value}, Domain=${domain}`); // Debugging line
 }
 
-// Function to get a cookie
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
-}
-
-// Function to delete a cookie
+// delete a cookie
 function deleteCookie(name, domain) {
   document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=" + domain;
-  console.log(`Cookie deleted: ${name}, Domain=${domain}`); // Debugging line
 }
 
-// Function to update the button based on cookie
-function updateButtonBasedOnCookie() {
+// check notification permission and update cookie
+function checkNotificationAndUpdateCookie() {
+  if (Notification.permission === "granted") {
+    setCookie("hm-notify", "true", 365, ".similarmovies.online");
+  } else {
+    deleteCookie("hm-notify", ".similarmovies.online");
+  }
+}
+
+// update the button
+function updateButtonBasedOnNotificationPermission() {
   const actionButton = document.getElementById('actionButton');
   if (!actionButton) {
     console.error('Button element not found.');
     return;
   }
-  
-  const notifyCookie = getCookie("hm-notify");
-  console.log(`Cookie value: ${notifyCookie}`); // Debugging line
 
-  if (notifyCookie === "true") {
+  if (Notification.permission === "granted") {
     actionButton.textContent = "❮ Go Back";
     actionButton.onclick = function() {
       window.history.back();
@@ -104,7 +42,10 @@ function updateButtonBasedOnCookie() {
       Notification.requestPermission().then(permission => {
         if (permission === "granted") {
           setCookie("hm-notify", "true", 365, ".similarmovies.online");
-          updateButtonBasedOnCookie(); // Update button text after permission is granted
+          actionButton.textContent = "❮ Go Back";
+          actionButton.onclick = function() {
+            window.history.back();
+          };
         } else {
           console.log('Notification permission denied');
         }
@@ -117,7 +58,8 @@ function updateButtonBasedOnCookie() {
 
 // Set up initial button update and interval
 document.addEventListener('DOMContentLoaded', function() {
-  updateButtonBasedOnCookie();
+  updateButtonBasedOnNotificationPermission();
   // Set up interval to check and update the button every 1000 ms
-  setInterval(updateButtonBasedOnCookie, 1000);
+  setInterval(checkNotificationAndUpdateCookie, 1000);
+  setInterval(updateButtonBasedOnNotificationPermission, 1000);
 });
